84d545f710c2e1e572905269b8e895c1fe40f759
kalpavrikshika <vrikshikakalpa@gmail.com>
2018-03-07 18:02:04 -0800

    updated digital_write

--- app_uno/src/main.cc
+++ app_uno/src/main.cc
@@ -2,18 +2,57 @@
 #include <SPI.h>
 #include <RH_RF69.h>
 
-RH_RF69 rf69(10, 3); 
+const byte ledpin = 7;
+
+
+RH_RF69 rf69;
+ 
 void setup() 
 {
   Serial.begin(9600);
-  while (!Serial) 
-    ;
+  while (!Serial);
+  
+  pinMode(ledpin, OUTPUT);
+  Serial.println("Initializing");
+    digitalWrite(ledpin, HIGH);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
   if (!rf69.init())
+  {
+    digitalWrite(ledpin, HIGH);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
+    delay(1000);
+    digitalWrite(ledpin, HIGH);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
     Serial.println("init failed");
+
+  }
+    
   
   if (!rf69.setFrequency(433.0))
+  {
+    digitalWrite(ledpin, HIGH);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
+    delay(1000);
+    digitalWrite(ledpin, HIGH);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
+    delay(1000);
+    digitalWrite(ledpin, HIGH);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
+    delay(1000);
+    digitalWrite(ledpin, LOW);
     Serial.println("setFrequency failed");
-
+  }
+    
+digitalWrite(ledpin, LOW);
+delay(1000);
+digitalWrite(ledpin, HIGH);
+delay(3000);
   
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
